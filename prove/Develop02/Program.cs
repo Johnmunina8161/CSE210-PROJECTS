1ba508879db69823c3093cba7ddda9d0fb1bfe7a
John Munina <97455093+Johnmunina8@users.noreply.github.com>
2023-05-06 07:25:29 +0200

    Program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -136,7 +136,7 @@ namespace DailyJournal
             } Console.WriteLine();
 }   }   } 
 
-                            // Program diagram //
+                            // Program Diagram 
 
 /*
                             +-----------------+
