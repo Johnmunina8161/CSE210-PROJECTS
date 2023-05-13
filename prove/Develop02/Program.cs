c1437324faef293043a5e3a121cc99161e1641d6
John Munina <97455093+Johnmunina8@users.noreply.github.com>
2023-05-13 07:46:56 +0200

    Program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -58,7 +58,7 @@ class Program
         } 
 
     }
-                            // Diagram  Program
+                            // Diagram Program
 
   /*
                             +-----------------+
