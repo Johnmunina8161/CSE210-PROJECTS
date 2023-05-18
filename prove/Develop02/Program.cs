b4ef70c40ed2633e9682e4b3182cf44cbe006fd8
John Munina <97455093+Johnmunina8@users.noreply.github.com>
2023-05-18 14:09:45 +0200

    Program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,42 +1,53 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 
-namespace DailyJournal;
-class Program
+namespace DailyJournal
+{
+    class Program
     {
         static void Main(string[] args)
         {
             Journal journal = new Journal();
 
-            // List of prompts
             List<string> prompts = new List<string>
             {
-                "what is your first name? ",
-                "What is your last name? ",
-                "How was your day? ",
-                "how did you feel after prayer? ",
-                "What is your favorite color? ",
+               "What frightened you today?",
+               "Who do you wish you had talked to today? What would you say?",
+               "What is one thing you want to remember from today?",
+               "What steps did you take today towards a goal you’re working on?",
+               "What do you need the most right now?",
+               "If you could only accomplish three things tomorrow, what would they be?",
+               "What was the most peaceful moment during the day?", 
+               "What is something that you would like to change about yourself? How can you change it?",
+               "How can you know if inspiration is coming from God or your own thoughts?",
+               "Why do you keep asking yourself to write down what you are learning?",
+               "What active learning techniques will you start to use?",
+               "Why is teaching a requirement for disciples of Christ?",
+               "Why should payment of tithes and offerings take priority over the payment of living expenses?",
+               "How does the self-reliant approach to budgeting help you to become a better steward over your finances?",
+               "What blessings have come to you as either a giver or receiver of generous fast offerings?",
+
+
             };
 
             bool quit = false;
             while (!quit)
             {
-                Console.WriteLine("\nDaily Journal Menu");
-                Console.WriteLine("------------");
-                Console.WriteLine("A. new entry");
-                Console.WriteLine("B. Display journal");
-                Console.WriteLine("C. Save the journal to a file");
-                Console.WriteLine("D. Load the journal from a file");
+                Console.WriteLine("\nWelcome to the Journal Program");
+                Console.WriteLine("Please Select one of the following choices:");
+                Console.WriteLine("A. Write");
+                Console.WriteLine("B. Display");
+                Console.WriteLine("C. Save");
+                Console.WriteLine("D. Load");
                 Console.WriteLine("E. Quit");
 
-                Console.Write("\nEnter your choice from (1-5): ");
+                Console.WriteLine("\nWhat would you like to do? ");
                 string choice = Console.ReadLine();
 
                 switch (choice)
                 {
                     case "A":
-                        journal.AddEntry(prompts[new Random().Next(prompts.Count)]);
+                        string prompt = GetRandomPrompt(prompts);
+                        journal.AddEntry(prompt);
                         break;
                     case "B":
                         journal.DisplayEntries();
@@ -54,39 +65,15 @@ class Program
                         Console.WriteLine("Invalid choice.");
                         break;
                 }
-            }   Console.WriteLine();
-        } 
+            }
+            Console.WriteLine("continue!");
+        }
 
+        static string GetRandomPrompt(List<string> prompts)
+        {
+            Random random = new Random();
+            int index = random.Next(prompts.Count);
+            return prompts[index];
+        }
     }
-                            // Diagram Program
-
-  /*
-                            +-----------------+
-                            |     Program     |
-                            +-----------------+
-                            |   - journal     |
-                            +-----------------+
-                                    |
-                                    |
-                                    |
-                        +-----------+-----------+
-                        |                       |
-                 +------+--------+      +-------+-----+
-                 |  PromptList   |      |     Entry   |
-                 +---------------+      +-------------+
-                 | -prompts      |      | -prompt     |
-                 |               |      | -response   |
-                 | +get_prompt() |      | -date       |
-                 +---------------+      +-------------+
-                                    |
-                                    |
-                      +-------------+-------------+
-                      |             |             |
-            +---------+-----+ +-----+---------+ +------------+
-            |   SaveToFile  | |  LoadFromFile | |  Display   |
-            +---------------+ +---------------+ +------------+
-            | -filename     | | -filename     | | -journal   |
-            |               | |               | |            |
-            | +save()       | | +load()       | | +display() |
-            +---------------+ +---------------+ +------------+     */    
-
+}
