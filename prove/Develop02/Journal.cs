e62d6d27481e46cc984cd0a3a43f08252a42cfc4
John Munina <97455093+Johnmunina8@users.noreply.github.com>
2023-05-18 14:08:20 +0200

    Journal

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,13 +1,14 @@
 using System;
+
 namespace DailyJournal
 {
     class Journal
     {
-        private List<Entry> entries;
+        public List<Entry> entries;
 
         public Journal()
         {
-          List<Entry>_entries = new List<Entry>();
+            entries = new List<Entry>();
         }
 
         public void AddEntry(string prompt)
@@ -25,38 +26,49 @@ namespace DailyJournal
                 Console.WriteLine(entry.ToString());
             }
         }
-
         public void SaveToFile()
         {
             Console.Write("Enter Filename: ");
             string filename = Console.ReadLine();
+
             using (StreamWriter writer = new StreamWriter(filename))
             {
+                writer.WriteLine("Date,Prompt,Response");
+
                 foreach (Entry entry in entries)
                 {
-                    writer.WriteLine($"{entry.GetDate()},{entry.GetPrompt()},{entry.GetResponse()}");
+                    writer.WriteLine($"{entry.GetDate()},{entry.GetPrompt().Replace(",", ",,")},{entry.GetResponse().Replace(",", ",,")}");
                 }
             }
+
             Console.WriteLine("Saved");
         }
 
         public void LoadFromFile()
         {
+             // Method to create a file txt
             Console.Write("Enter filename: ");
             string filename = Console.ReadLine();
             entries.Clear();
+
             using (StreamReader reader = new StreamReader(filename))
             {
+                string headerLine = reader.ReadLine(); 
+
                 while (!reader.EndOfStream)
                 {
-                    string[] fields = reader.ReadLine().Split(',');
+                    string entryLine = reader.ReadLine();
+                    string[] fields = entryLine.Split(',');
+
                     string date = fields[0];
-                    string prompt = fields[1];
-                    string response = fields[2];
+                    string prompt = fields[1].Replace(",,", ",");
+                    string response = fields[2].Replace(",,", ",");
+
                     entries.Add(new Entry(prompt, response, date));
                 }
             }
-            Console.WriteLine("Loaded");
+
+            Console.WriteLine("done");
         }
     }
 }
