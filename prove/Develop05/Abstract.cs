07f7dd7c9c73253830e44e0ba3839037864500d2
John Munina <97455093+Johnmunina8@users.noreply.github.com>
2023-06-21 02:08:43 +0200

    Goals

---
+++ prove/Develop05/Abstract.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace gamification 
+{
+        
+    // Base class for all goals,  This class will be the base class for all other goals in the system.
+    public abstract class Goal
+    {
+        public string _Description ; //field stores the description of the goal.
+        public bool _IsCompleted ; // field stores whether the goal has been completed.
+        public int _Score ; //  field stores the score of the goal.
+
+        public Goal(string description) // constructor initializes the goal with the given description
+        {
+            _Description = description;
+            _IsCompleted = false;
+            _Score = 0;
+        }
+
+        public abstract void Complete(); // abstract method that must be implemented by all subclasses of Goal. This method will be used to complete the goal.
+    }
+}
---
+++ prove/Develop05/Program-class.cs
@@ -0,0 +1,161 @@
+namespace gamification 
+{
+    // Main program class
+    public class Program
+    {
+        private static List<Goal> goals = new List<Goal>(); // A list that stores instances of the Goal clas & totalScore which holds the cumulative score of the user.
+        private static int totalScore = 0;
+
+        private static void Main() //this method is the entry point of the program. It first calls the LoadGoals() method to load any previously saved goals and scores
+        {
+            LoadGoals(); 
+
+            bool exitProgram = false;
+
+            while (!exitProgram)
+            {
+                Console.WriteLine("Please select one of the following options: ");
+                Console.WriteLine("1. Create a new goal");
+                Console.WriteLine("2. Record an event");
+                Console.WriteLine("3. Display goals");
+                Console.WriteLine("4. Display total score");
+                Console.WriteLine("5. Save goals and score");
+                Console.WriteLine("6. Exit");
+                Console.Write("Enter your choice: ");
+                string choice = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        CreateGoal();
+                        break;
+                    case "2":
+                        RecordEvent();
+                        break;
+                    case "3":
+                        DisplayGoals();
+                        break;
+                    case "4":
+                        DisplayTotalScore();
+                        break;
+                    case "5":
+                        SaveGoals();
+                        break;
+                    case "6":
+                        exitProgram = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void CreateGoal()
+        {
+            Console.WriteLine("Select goal type:");
+            Console.WriteLine("1. Simple goal");
+            Console.WriteLine("2. Eternal goal");
+            Console.WriteLine("3. Checklist goal");
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            Console.Write("Enter goal description: ");
+            string description = Console.ReadLine();
+            Console.WriteLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Enter points for completing the goal: ");
+                    int points = int.Parse(Console.ReadLine());
+                    goals.Add(new SimpleGoal(description, points));
+                    break;
+                case "2":
+                    Console.Write("Enter points for each recording of the goal: ");
+                    int eternalPoints = int.Parse(Console.ReadLine());
+                    goals.Add(new EternalGoal(description, eternalPoints));
+                    break;
+                case "3":
+                    Console.Write("Enter target count for the checklist goal: ");
+                    int targetCount = int.Parse(Console.ReadLine());
+                    Console.Write("Enter points for each recording of the goal: ");
+                    int checklistPoints = int.Parse(Console.ReadLine());
+                    goals.Add(new ChecklistGoal(description, targetCount, checklistPoints));
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Goal creation cancelled.");
+                    break;
+            }
+
+            Console.WriteLine("Goal created successfully.");
+        }
+
+        private static void RecordEvent()
+        {
+            if (goals.Count == 0)
+            {
+                Console.WriteLine("No goals found. Please create a goal first.");
+                return;
+            }
+
+            Console.WriteLine("Select a goal to record an event:");
+            for (int i = 0; i < goals.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {goals[i]._Description}");
+            }
+
+            Console.Write("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine()) - 1;
+
+            if (choice >= 0 && choice < goals.Count)
+            {
+                Goal selectedGoal = goals[choice];
+                selectedGoal.Complete();
+                Console.WriteLine("Event recorded successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Event recording cancelled.");
+            }
+        }
+
+        private static void DisplayGoals()
+        {
+            Console.WriteLine("List of Goals:");
+
+            for (int i = 0; i < goals.Count; i++)
+            {
+                Goal goal = goals[i];
+
+                Console.Write($"{i + 1}. [{(goal._IsCompleted ? "X" : " ")}] {goal._Description}");
+
+                if (goal is ChecklistGoal checklistGoal)
+                {
+                    Console.Write($" - {checklistGoal.GetProgressStatus()}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void DisplayTotalScore()
+        {
+            Console.WriteLine($"Total Score: {totalScore}");
+        }
+
+        private static void SaveGoals()
+        {
+            // Save goals and totalScore to a file or database
+            Console.WriteLine("Goals and score saved successfully.");
+        }
+
+        private static void LoadGoals()
+        {
+            // Load goals and totalScore from a file or database
+           Console.WriteLine("Goals and score loaded successfully.");
+        }
+    }
+}
