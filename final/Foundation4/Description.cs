465ec19363d585c3dad15d2b6c9631ddd24e7f0d
John Munina <97455093+Johnmunina8@users.noreply.github.com>
2023-07-15 06:38:19 +0200

    polymorphysm

---
+++ final/Foundation4/Description.cs
@@ -0,0 +1,10 @@
+/*
+
+The program demonstrates the concept of 
+polymorphism. It defines a hierarchy of activity types (Running, Cycling, and Swimming) 
+that inherit from a base class called Activity, 
+and allows different activity types to share a common interface (Activity) 
+while providing specialized behavior through method overriding.
+
+
+*/ 
