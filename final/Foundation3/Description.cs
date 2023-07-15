8fdbfa9e78c554b540643c4159dea520ec9334c2
John Munina <97455093+Johnmunina8@users.noreply.github.com>
2023-07-15 06:15:39 +0200

    Inheritance

---
+++ final/Foundation3/Description.cs
@@ -0,0 +1,8 @@
+/*
+the program showcases the use of inheritance 
+to create specialized event classes that inherit common functionality 
+from the base Event class. It demonstrates polymorphism by 
+invoking the overridden GenerateMarketingMessage() 
+method for each specific event type.
+
+*/
